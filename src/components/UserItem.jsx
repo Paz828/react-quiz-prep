de2dd1d721d66225fce1cb03d8d2d8257fcd1b3e
Dorion Boesch <dorion.boesch@gmail.com>
2023-07-14 10:42:58 -0500

    added email p tags

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -4,8 +4,9 @@ const UserItem = ({ user }) => {
   };
 
   return (
-    <div id={user.id} onClick={handleClick} style={{ padding: ".5%" }}>
-      <h1>{user.name}</h1>
+    <div id={user.id} onClick={handleClick} style={{ padding: ".25%" }}>
+      <h1 style={{ padding: ".25%" }}>{user.name}</h1>
+      <p>{user.email}</p>
     </div>
   );
 };
